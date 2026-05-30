2b4d51f966aa20dc607d8703b2cf2fedba05c9b6
ljimark2111-pixel <ljimark2111@gmail.com>
2026-05-30 23:47:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+body{
+  background:#111;
+  color:white;
+  text-align:center;
+  font-family:Arial,sans-serif;
+  padding-top:50px;
+}
 
+h1{
+  color:gold;
+}
+
+.score{
+  font-size:30px;
+  margin:20px;
+}
+
+#tapBtn{
+  width:200px;
+  height:200px;
+  border-radius:50%;
+  font-size:35px;
+  background:gold;
+  border:none;
+}
